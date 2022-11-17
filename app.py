50cfa95d78854153b592cad593421c682cf3a883
Debora Lee <sayapinn1@gmail.com>
2022-11-17 12:18:14 +0900

    app.py 수정

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.hrta48t.mongodb.net/Cluster0?retryWrites=true&w=majority')
+import certifi
+
+ca = certifi.where()
+client = MongoClient('mongodb+srv://sayapinn:$100doller@cluster0.qv0cojm.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('haebeen_index.html')
+   return render_template('index.html')
 
 @app.route("/first_mini", methods=["POST"])
 def first_mini_post():
@@ -19,15 +22,14 @@ def first_mini_post():
         'comment':comment_receive
     }
 
-    db.first_mini.insert_one(doc)
+    db.intro.insert_one(doc)
 
     return jsonify({'msg':'저장 완료'})
 
 @app.route("/first_mini", methods=["GET"])
 def first_mini_get():
-    homework_list = list(db.first_mini.find({}, {'_id': False}))
+    homework_list = list(db.intro.find({}, {'_id': False}))
     return jsonify({'first_mini':homework_list})
-
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
