fb33f249d06d3ff6ad73bd18cbf5cd60b700e8bc
Jo haebeen <haebin1622@naver.com>
2022-11-17 11:55:48 +0900

    v.1.0.2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ db = client.dbsparta
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('haebeen_index.html')
 
 @app.route("/first_mini", methods=["POST"])
 def first_mini_post():
