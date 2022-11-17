80fc2a6b8ca98ee10a77c8f03f3312d12263c71f
Jo haebeen <haebin1622@naver.com>
2022-11-17 12:55:37 +0900

    v.1.0.5

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ ca = certifi.where()
 client = MongoClient('mongodb+srv://sayapinn:$100doller@cluster0.qv0cojm.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
+# 보라님
 @app.route('/')
 def home():
-   return render_template('index.html')
+   return render_template('Debora_index.html')
 
 @app.route("/first_mini", methods=["POST"])
 def first_mini_post():
@@ -33,3 +34,119 @@ def first_mini_get():
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
+
+# 봉진님
+
+@app.route('/')
+def home():
+   return render_template('bongjin_index.html')
+
+@app.route("/first_mini", methods=["POST"])
+def first_mini_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+
+    db.bongjin.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료'})
+
+@app.route("/first_mini", methods=["GET"])
+def first_mini_get():
+    homework_list = list(db.bongjin.find({}, {'_id': False}))
+    return jsonify({'first_mini':homework_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
+
+
+# 해빈님
+
+@app.route('/')
+def home():
+   return render_template('haebeen_index.html')
+
+@app.route("/first_mini", methods=["POST"])
+def first_mini_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+
+    db.haebeen.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료'})
+
+@app.route("/first_mini", methods=["GET"])
+def first_mini_get():
+    homework_list = list(db.haebeen.find({}, {'_id': False}))
+    return jsonify({'first_mini':homework_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
+
+
+# 종수님
+
+@app.route('/')
+def home():
+   return render_template('jongsu_index.html')
+
+@app.route("/first_mini", methods=["POST"])
+def first_mini_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+
+    db.jongsu.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료'})
+
+@app.route("/first_mini", methods=["GET"])
+def first_mini_get():
+    homework_list = list(db.jongsu.find({}, {'_id': False}))
+    return jsonify({'first_mini':homework_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
+
+
+# 예빈님
+
+@app.route('/')
+def home():
+   return render_template('yebin_index.html')
+
+@app.route("/first_mini", methods=["POST"])
+def first_mini_post():
+    name_receive = request.form['name_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'name':name_receive,
+        'comment':comment_receive
+    }
+
+    db.yebin.insert_one(doc)
+
+    return jsonify({'msg':'저장 완료'})
+
+@app.route("/first_mini", methods=["GET"])
+def first_mini_get():
+    homework_list = list(db.yebin.find({}, {'_id': False}))
+    return jsonify({'first_mini':homework_list})
+
+if __name__ == '__main__':
+   app.run('0.0.0.0', port=5000, debug=True)
+
